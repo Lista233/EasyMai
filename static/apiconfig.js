f1a33dd4b8790175751bd909e928f644cef263f3
Lista233 <1312597957@qq.com>
2025-03-30 00:14:35 +0800

    移除配置文件

--- static/apiconfig.js
+++
@@ -1,4 +0,0 @@
-export const remoteRoute='https://mai.lista233.cn';
-export const aliasRoute='https://api.yuzuchan.moe/maimaidx/maimaidxalias'
-export const version='20250329'
-export const apiversion='20250329'
